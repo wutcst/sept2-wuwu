413085792f288bb42ea5c0c3591b0fa3e60de87c
王煜泽 <2972079547@qq.com>
2023-06-14 22:31:00 +0800

    springboot项目正常启动

---
+++ src/main/java/com/wuwu/worldofzuulwuwu/WorldOfZuulWuwuApplication.java
@@ -0,0 +1,13 @@
+package com.wuwu.worldofzuulwuwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldOfZuulWuwuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldOfZuulWuwuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wuwu/worldofzuulwuwu/WorldOfZuulWuwuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuwu.worldofzuulwuwu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldOfZuulWuwuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
