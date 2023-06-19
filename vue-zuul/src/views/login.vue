fae5b75ad854ff65d99900c016d3fe69b36baf99
ccccn_Lin <2864710463@qq.com>
2023-06-19 19:39:37 +0800

    前端 login测试post请求 v2.0

--- vue-zuul/src/views/login.vue
+++ vue-zuul/src/views/login.vue
@@ -1,7 +1,9 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div style="width: 100%;height: 100vh; background: linear-gradient(90deg,#3cadeb,#9c88ff);display: flex;justify-content: center;align-items: center">
-    <div style="width: 300px; height: 380px;box-shadow: 0 5px 15px rgba(0, 0, 0, .8); display: flex;justify-content: center;align-items: center;">
+  <div
+    style="width: 100%;height: 100vh; background: linear-gradient(90deg,#3cadeb,#9c88ff);display: flex;justify-content: center;align-items: center">
+    <div
+      style="width: 300px; height: 380px;box-shadow: 0 5px 15px rgba(0, 0, 0, .8); display: flex;justify-content: center;align-items: center;">
       <!-- :model="form"：该属性用于将表单数据绑定到组件的form属性上，实现表单数据的双向绑定。 -->
       <!-- :rules="rules"：该属性用于将表单的验证规则绑定到组件的rules属性上 -->
       <el-form :model="form" size="normal" style="height: 100% ;width: 220px;" ref="form" :rules="rules">
@@ -19,7 +21,9 @@
         </el-form-item>
         <!-- 登录 -->
         <el-form-item>
-          <el-button style="color: white; border-radius: 30px;width: 100%;height: 50px;background: linear-gradient(90deg,#3cadeb,#9c88ff);" @click="login">登录</el-button>
+          <el-button
+            style="color: white; border-radius: 30px;width: 100%;height: 50px;background: linear-gradient(90deg,#3cadeb,#9c88ff);"
+            @click="login">登录</el-button>
         </el-form-item>
         <!-- 注册 -->
         <el-form-item style="text-align: center; color: white;">
@@ -58,7 +62,7 @@ export default {
   created() {
     sessionStorage.removeItem("user");
   },
-  
+
   methods: {
     toRegister() {
       this.$router.push("/register"); //跳转到注册界面
@@ -66,21 +70,22 @@ export default {
     login() {
       this.$refs["form"].validate((valid) => {
         if (valid) {
-          axios.get('http://yapi.smart-xwork.cn/mock/264710/yapi/login1').then((result) => {
+          /* login界面发送post请求更合适，传表单数据的username和password给后端 */
+          axios.post('http://yapi.smart-xwork.cn/mock/264710/yapi/login1', { "username": this.form.username, 'password': this.form.password }).then((result) => {
             console.log(result);
-            if(result.data.code === 0){
+            if (result.data.code === 1) {
               this.$message({
                 type: "success",
                 message: "登录成功",
               });
               this.$router.push("/main"); //登录成功后的页面跳转到主界面
             }
-            else{
+            else {
               /* this.$router.push("/main"); */
               /* alert("用户名或密码错误。") */
               this.$message({
                 type: "error",
-                message: result.data.message,
+                message: "用户名或密码错误",
               });
             }
           }).catch((err) => {
@@ -98,6 +103,5 @@ export default {
 
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
 
