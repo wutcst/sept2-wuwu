a05a378e8b191add9caa9e01eafd9acdaa216441
王煜泽 <2972079547@qq.com>
2023-06-22 20:01:14 +0800

    后端 项目数据库实体类的完善 v1.0

--- src/main/java/com/wuwu/worldofzuulwuwu/entity/Player.java
+++ src/main/java/com/wuwu/worldofzuulwuwu/entity/Player.java
@@ -1,13 +1,16 @@
 package com.wuwu.worldofzuulwuwu.entity;
 
+import lombok.Data;
+
 /**
  * @author:wangyuze
  * @create: 2023-06-22 14:18
  * @Description: 玩家类
  */
 
+@Data
 public class Player {
+    private Long Id;
     private String name;
     private String password;
-
 }
