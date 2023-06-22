36d1f24a9280815c6b6cf1029328699a5d7835b0
王煜泽 <2972079547@qq.com>
2023-06-22 21:34:01 +0800

    后端 PlayerItemDAO的完善 v1.0

---
+++ src/main/java/com/wuwu/worldofzuulwuwu/mapper/PlayerItemDao.java
@@ -0,0 +1,27 @@
+package com.wuwu.worldofzuulwuwu.mapper;
+
+import com.wuwu.worldofzuulwuwu.entity.Player;
+import com.wuwu.worldofzuulwuwu.entity.PlayerItem;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface PlayerItemDao {
+
+    @Select("SELECT * FROM playerItem WHERE playerId=#{playerId}  AND itemId=#{id}")
+    PlayerItem findOne(Long playerId,@Param("id") Integer itemId);
+
+    @Insert("INSERT INTO playerItem VALUES (#{playerId},#{id},#{count})")
+    PlayerItem save(PlayerItem playerItem);
+
+    @Select("SELECT * FROM playerItem")
+    List<PlayerItem> findAll();
+
+    @Update("UPDATE playerItem WHERE playerId=#{playerId} AND id=#{id}")
+    int update(PlayerItem playerItem);
+
+    @Delete("DELETE playerItem WHERE playerId=#{playerId} AND id=#{id}")
+    int delete(PlayerItem playerItem);
+
+}
---
+++ src/main/java/com/wuwu/worldofzuulwuwu/mapper/RoomItemDao.java
@@ -0,0 +1,4 @@
+package com.wuwu.worldofzuulwuwu.mapper;
+
+public interface RoomItemDao {
+}
