f8303d59a68b19fefeeb35867cf1262a9d150f6b
wjycn2000 <wjy.cn2000@outlook.com>
2023-06-22 13:55:47 +0800

    后端 完成items功能1.0

--- src/main/java/zuul/Room.java
+++ src/main/java/zuul/Room.java
@@ -1,17 +1,21 @@
 package zuul;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 public class Room
 {
     private String description;
     private HashMap<String, Room> exits;        // stores exits of this room.
+    private List<Item> items;
 
     public Room(String description)
     {
         this.description = description;
         exits = new HashMap<>();
+        items = new LinkedList<>();
     }
 
     public void setExit(String direction, Room neighbor)
@@ -42,6 +46,18 @@ public class Room
     public Room getExit(String direction)
     {
         return exits.get(direction);
+    }
+
+    public String getItemsString(){
+        if(items.isEmpty()){
+            return "This room has been cleared.";
+        } else{
+            String description = "After a minute of search, you find:\n";
+            for(Item item : items){
+                description += item.getName() + " *" + item.getDescription() + "\n";
+            }
+            return description;
+        }
     }
 }
 
