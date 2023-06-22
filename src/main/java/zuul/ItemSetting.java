3a15f76f61ad9ed32c45fa07f4ad716f6271a60f
wjycn2000 <wjy.cn2000@outlook.com>
2023-06-22 22:50:09 +0800

    后端 完善items功能2.0

---
+++ src/main/java/zuul/ItemSetting.java
@@ -0,0 +1,41 @@
+package zuul;
+
+import java.util.HashMap;
+
+public class ItemSetting {
+
+  private static HashMap<String, Integer> itemIdMap;
+  private static HashMap<Integer, Item> itemMap;
+
+  static {
+    itemMap.put(1, new Item("gem", "This gem looks worth 100 million.", 1));
+    itemMap.put(2, new Item("gold cup", "This gold cup used to contain holy water", 4));
+    itemMap.put(3, new Item("cookie1", "The cookie can give you power.", 1));
+    itemMap.put(4, new Item("lantern", "A tool that can allow you see things in darkness.", 2));
+    itemMap.put(5, new Item("crown", "It seemed to belong to a king.", 2));
+    itemMap.put(6, new Item("cookie2", "The cookie can give you power.", 1));
+    itemMap.put(7, new Item("rune", "The rune will give you knowledge of ancient magic", 3));
+    itemMap.put(8, new Item("sword", "A weapon that can kill a human, or a dead human.", 4));
+    itemMap.put(9, new Item("ring", "The legendary magic ring used to be owned by a Hobbit", 1));
+
+    itemIdMap.put("gem", 1);
+    itemIdMap.put("gold cup", 2);
+    itemIdMap.put("cookie1", 3);
+    itemIdMap.put("lantern", 4);
+    itemIdMap.put("crown", 5);
+    itemIdMap.put("cookie2", 6);
+    itemIdMap.put("rune", 7);
+    itemIdMap.put("sword", 8);
+    itemIdMap.put("ring", 9);
+  }
+
+  public static Integer getId(String itemName){
+    return itemIdMap.get(itemName);
+  }
+
+  public static Item getItem(Integer itemId){
+    return itemMap.get(itemId);
+  }
+
+
+}
