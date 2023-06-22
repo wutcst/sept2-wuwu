cabbe267945bcc03903f05373b23a724b8964f01
王煜泽 <2972079547@qq.com>
2023-06-22 21:54:59 +0800

    后端 数据库配置 v1.0

--- src/test/java/com/wuwu/worldofzuulwuwu/WorldOfZuulWuwuApplicationTests.java
+++ src/test/java/com/wuwu/worldofzuulwuwu/WorldOfZuulWuwuApplicationTests.java
@@ -1,10 +1,26 @@
 package com.wuwu.worldofzuulwuwu;
 
+import com.wuwu.worldofzuulwuwu.entity.Player;
+import com.wuwu.worldofzuulwuwu.entity.PlayerItem;
+import com.wuwu.worldofzuulwuwu.mapper.PlayerDao;
+import com.wuwu.worldofzuulwuwu.mapper.PlayerItemDao;
+import com.wuwu.worldofzuulwuwu.mapper.RoomItemDao;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WorldOfZuulWuwuApplicationTests {
+
+	@Autowired
+	PlayerDao playerDao;
+
+	@Autowired
+	PlayerItemDao playerItemDao;
+
+	@Autowired
+	RoomItemDao roomItemDao;
+
 
 	@Test
 	void contextLoads() {
