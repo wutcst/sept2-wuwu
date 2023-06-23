b8cdb92f2ccc5d01f18615a38d3907b92e4e3178
王煜泽 <2972079547@qq.com>
2023-06-23 13:00:54 +0800

    后端 service层完善 v1.0

--- src/main/java/zuul/RoomId.java
+++ src/main/java/zuul/RoomId.java
@@ -1,6 +1,17 @@
 package zuul;
 
 public enum RoomId {
-    ENTRANCE, COOKIE_ROOM, CAVE, TREASURE_ROOM, DARKROOM, LOBBY, WEAPON_ROOM, JAIL, READING_ROOM, SECRET_ROOM,
-    EXIT1, EXIT2, EXIT3, EXIT4, EXIT5, EXIT6, EXIT7
+    ENTRANCE(0), COOKIE_ROOM(1), CAVE(2), TREASURE_ROOM(3), DARKROOM(4), LOBBY(5), WEAPON_ROOM(6), JAIL(7), READING_ROOM(8), SECRET_ROOM(9),
+    EXIT1(10), EXIT2(11), EXIT3(12), EXIT4(13), EXIT5(14), EXIT6(15), EXIT7(16);
+    private Integer id;
+    RoomId(){}
+    RoomId(int _id){
+        id=_id;
+    }
+    public Integer getId(){
+        return id;
+    }
+    public static Integer getRoomId(String name){
+        return RoomId.valueOf(name.toUpperCase()).getId();
+    }
 }
