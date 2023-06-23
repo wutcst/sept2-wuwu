ad1d46fcf1ecc5ccb4b6a4bb206eefb0158ea711
王煜泽 <2972079547@qq.com>
2023-06-23 13:03:31 +0800

    后端 service层完善 v1.0

--- src/main/java/com/wuwu/worldofzuulwuwu/service/impl/PlayerItemService.java
+++ src/main/java/com/wuwu/worldofzuulwuwu/service/impl/PlayerItemService.java
@@ -3,10 +3,12 @@ package com.wuwu.worldofzuulwuwu.service.impl;
 import com.wuwu.worldofzuulwuwu.entity.Player;
 import com.wuwu.worldofzuulwuwu.entity.PlayerItem;
 import com.wuwu.worldofzuulwuwu.mapper.PlayerItemDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import zuul.ItemSetting;
 
 public class PlayerItemService implements com.wuwu.worldofzuulwuwu.service.PlayerItemService {
 
+  @Autowired
   private PlayerItemDao playerItemDao;
 
   public Boolean addItem(Long playerId, String item){
--- src/main/java/com/wuwu/worldofzuulwuwu/service/impl/RoomItemService.java
+++ src/main/java/com/wuwu/worldofzuulwuwu/service/impl/RoomItemService.java
@@ -4,12 +4,15 @@ import com.wuwu.worldofzuulwuwu.entity.Player;
 import com.wuwu.worldofzuulwuwu.entity.PlayerItem;
 import com.wuwu.worldofzuulwuwu.entity.RoomItem;
 import com.wuwu.worldofzuulwuwu.mapper.RoomItemDao;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import zuul.ItemSetting;
 import zuul.Room;
 import zuul.RoomId;
 import zuul.RoomSetting;
 
 public class RoomItemService implements com.wuwu.worldofzuulwuwu.service.RoomItemService {
+  @Autowired
   private RoomItemDao roomItemDao;
 
   public Boolean addItem(Long playerId, String item){
@@ -29,7 +32,6 @@ public class RoomItemService implements com.wuwu.worldofzuulwuwu.service.RoomIte
     roomItem.setPlayerId(playerId);
     roomItem.setRoomId(RoomId.getRoomId(room.getName()));
     roomItem.setId(ItemSetting.getId(itemName));
-
     int result = roomItemDao.delete(roomItem);
 
     return result > 0;
