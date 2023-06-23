4ca8c53cdd1ca8dc75ec824c79d354ae3392363a
wjycn2000 <wjy.cn2000@outlook.com>
2023-06-23 15:28:42 +0800

    后端 完善LookCommand指令2.0

--- src/main/java/zuul/LookCommand.java
+++ src/main/java/zuul/LookCommand.java
@@ -1,12 +1,32 @@
 package zuul;
 
+import com.wuwu.worldofzuulwuwu.entity.RoomItem;
+import com.wuwu.worldofzuulwuwu.service.PlayerService;
+import com.wuwu.worldofzuulwuwu.service.RoomItemService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class LookCommand extends Command{
 
+  @Autowired
+  private RoomItemService roomItemService;
+  @Autowired
+  private PlayerService playerService;
+
   @Override
-  public boolean execute(Game game) {
+  public String execute(Long playerId) {
 
-    System.out.println(game.getCurrentRoom().getItems().getItemsString());
+    Integer currentRoomId = playerService.getCurrentRoom(playerId);
+    Room currentRoom = RoomSetting.rooms.get(RoomSetting.roomIds.get(currentRoomId));
 
-    return false;
+    List<RoomItem> roomItemList = roomItemService.lookUpAllItems(playerId, currentRoom.getName());
+    StringBuilder msg = new StringBuilder("You find:\n");
+    for(RoomItem roomItem : roomItemList){
+      Integer itemId = roomItem.getId();
+      Item item = ItemSetting.getItem(itemId);
+      msg.append("[").append(item.getName()).append("] [").append(item.getDescription())
+          .append("] [").append(item.getWeight()).append("]\n");
+    }
+    return msg.toString();
   }
 }
