4a503fec6624851b2d5de93b0459619960dd97c2
王煜泽 <2972079547@qq.com>
2023-06-23 20:40:33 +0800

    后端 sql文件上传 v1.0

---
+++ sql/player.sql
@@ -0,0 +1,1 @@
+INSERT INTO `player` (`id`, `name`, `password`, `currentRoom`, `capacity`) VALUES (1672196496054677506, 'xiaowang', '123456', 1, 10);
---
+++ sql/playeritem.sql
@@ -0,0 +1,1 @@
+INSERT INTO `playeritem` (`player_id`, `id`, `count`) VALUES (1672196496054677506, 1, 1);
