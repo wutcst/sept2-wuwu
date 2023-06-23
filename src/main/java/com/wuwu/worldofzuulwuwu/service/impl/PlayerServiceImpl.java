2d03dc4715a96f4df740384afe3f9aeb832f30be
wjycn2000 <wjy.cn2000@outlook.com>
2023-06-23 18:37:56 +0800

    后端 完善PlayerServiceImpl

--- src/main/java/com/wuwu/worldofzuulwuwu/service/impl/PlayerServiceImpl.java
+++ src/main/java/com/wuwu/worldofzuulwuwu/service/impl/PlayerServiceImpl.java
@@ -2,19 +2,32 @@ package com.wuwu.worldofzuulwuwu.service.impl;
 
 import com.wuwu.worldofzuulwuwu.entity.Player;
 import com.wuwu.worldofzuulwuwu.mapper.PlayerDao;
+import com.wuwu.worldofzuulwuwu.service.RoomItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class PlayerServiceImpl implements com.wuwu.worldofzuulwuwu.service.PlayerService {
 
   @Autowired
   private PlayerDao playerDao;
+  @Autowired
+  private RoomItemService roomItemService;
 
   public Boolean register(Player player){
     Player player0 = playerDao.findByName(player.getName());
     if(player0!=null){
       return false;
     }
-    return playerDao.save(player) > 0;
+    int result = playerDao.save(player);
+    roomItemService.addItem(player.getId(),"cave", "gem");
+    roomItemService.addItem(player.getId(),"cookie_room", "cookie1");
+    roomItemService.addItem(player.getId(),"treasure_room", "gold cup");
+    roomItemService.addItem(player.getId(),"lobby", "lantern");
+    roomItemService.addItem(player.getId(),"weapon_room", "sword");
+    roomItemService.addItem(player.getId(),"jail", "cookie2");
+    roomItemService.addItem(player.getId(),"jail", "crown");
+    roomItemService.addItem(player.getId(),"reading_room", "rune");
+    roomItemService.addItem(player.getId(),"exit6", "ring");
+    return  result> 0;
   }
 
   public Long login(Player player){
