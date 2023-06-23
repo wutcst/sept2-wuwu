37ef6eadae4f4f130fc1ae2f10d45f6d256877d1
王煜泽 <2972079547@qq.com>
2023-06-23 15:57:54 +0800

    后端 回退命令LookCommand的完善 v1.0

--- src/main/java/com/wuwu/worldofzuulwuwu/zuul/LookCommand.java
+++ src/main/java/com/wuwu/worldofzuulwuwu/zuul/LookCommand.java
@@ -9,4 +9,5 @@ public class LookCommand extends Command{
 
     return false;
   }
+
 }
--- src/main/java/com/wuwu/worldofzuulwuwu/zuul/Parser.java
+++ src/main/java/com/wuwu/worldofzuulwuwu/zuul/Parser.java
@@ -1,7 +1,10 @@
 package com.wuwu.worldofzuulwuwu.zuul;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Scanner;
 
+@Component
 public class Parser
 {
     private CommandWords commands;  // holds all valid command words
