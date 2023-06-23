c0291c8f6a8531e9452c4ef6c30e4d24daa23eb9
wangyuxuan <yyxx52673@126.com>
2023-06-23 14:05:00 +0800

    修改result结果类

---
+++ vue-zuul/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    
+    <!--    根据路由进行访问-->
+    <router-view></router-view>
+
+    <!-- <main-view></main-view> -->
+    <!-- <login-view></login-view> -->
+  </div>
+</template>
+
+
+<script>
+/* import MainView from './views/main.vue' */
+
+/* import LoginView from './views/login.vue' */
+export default {
+  /* name: "App", */
+  components: {/* MainView */ /* LoginView */
+
+  },
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+
+}
+</script>
+<style>
+
+</style>
